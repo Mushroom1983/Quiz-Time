e0b68e8cb3104c15e5f5257a6c2f9cdf4408ff58
Mushroom1983 <151859550+Mushroom1983@users.noreply.github.com>
2024-03-06 16:32:13 +0000

    Update logic.js
    setup import in import questions set up calls by ID

---
+++ starter/assets/JS/logic.js
@@ -0,0 +1,18 @@
+import { questions } from "./questions.js";
+
+const time = document.getElementById("time");
+
+const startScreen = document.getElementById("startScreen");
+const startButton = document.getElementById("start");
+
+const questionsContainer = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const choices = document.getElementById("choices");
+
+const endScreen = document.getElementById("endScreen");
+const finalScore = document.getElementById("finalScore");
+const initials = document.getElementById("initials");
+const submit = document.getElementById("submit");
+
+const feedback = document.getElementById("feedback");
+
